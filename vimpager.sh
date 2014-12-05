4a4d27f5bd706d2461db1269c6ebc1d8be313f23
nfnty <git@nfnty.se>
2014-12-05 11:41:27 +0100

    use separate script for manpager

--- vimpager.sh
+++ vimpager.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | col -x -b | vim -R -c 'set noswf ft=man nomod fdl=99' -
+sed -r 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g' | col -x -b | vim -R -c 'set noswf ft= nomod fdl=99' -
