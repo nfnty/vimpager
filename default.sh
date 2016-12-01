3e4fbda8772747539194661a7fa11f0c3ad2b5bd
nfnty <git@nfnty.se>
2016-12-01 21:48:15 +0100

    Remove `col`, Use perl full regex

--- default.sh
+++ default.sh
@@ -3,6 +3,5 @@
 set -o errexit -o noclobber -o noglob -o nounset -o pipefail
 IFS=$'\n'
 
-sed --regexp-extended 's/\x1B\[([0-9]{1,2}(;[0-9]{1,2})*)?[m|K]//g' | \
-    col --spaces --no-backspaces | \
+perl -p -e 's/(?:\x1B\x5B|\x9B)[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]//g' | \
     nvim -R -c 'set noswf nomod ft=' -
