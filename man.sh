598279818e22521dbb427b5fcb90bf73965c4885
nfnty <git@nfnty.se>
2016-12-22 05:04:43 +0100

    nvim: `filetype=man` needs to set nomodifiable itself

--- man.sh
+++ man.sh
@@ -7,4 +7,4 @@ if [[ "${#}" -gt 0 ]]; then
     separator='--'
 fi
 perl -p -e 's/(?:\x1B\x5B|\x9B)[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]//g' | \
-    nvim -R -c 'set noswapfile nomodifiable filetype=man' ${separator:-} "${@:--}"
+    nvim -R -c 'set noswapfile filetype=man' ${separator:-} "${@:--}"
