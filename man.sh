c8af80ef757f5a810983642840f8df18685cc200
nfnty <git@nfnty.se>
2016-12-22 03:59:47 +0100

    Filename arguments

--- man.sh
+++ man.sh
@@ -3,5 +3,8 @@
 set -o errexit -o noclobber -o noglob -o nounset -o pipefail
 IFS=$'\n'
 
+if [[ "${#}" -gt 0 ]]; then
+    separator='--'
+fi
 perl -p -e 's/(?:\x1B\x5B|\x9B)[\x30-\x3F]*[\x20-\x2F]*[\x40-\x7E]//g' | \
-    nvim -R -c 'set noswf nomod ft=man' -
+    nvim -R -c 'set noswf nomod ft=man' ${separator:-} "${@:--}"
